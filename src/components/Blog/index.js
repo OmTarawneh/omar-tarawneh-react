b651c3e5d790d0ad3b6aa7399bba3add5006ba0e
Omar Tarawneh <omar@omar-tarawneh-pc.scoiumtech.local>
2021-07-11 16:38:45 +0300

    adding the global blog posts

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -10,38 +10,52 @@ function Blog() {
   const [offset, setOffset] = useState(0);
   const [count, setCount] = useState(0);
   const [tag, setTag] = useState(null);
-
   useEffect(() => {
-    fetch(`http://localhost:4000/api/v1/blog?limit=5&offset=${offset}`, {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        setBlogPosts(result.data);
-        setCount(result.itemsCount);
-      })
-      .catch((err) => console.error(err.message));
+    const fetchData = async () => {
+      const response = await fetch(
+        `http://localhost:4000/api/v1/blog?limit=5&offset=${offset}`,
+        {
+          method: 'GET',
+          mode: 'cors',
+          cache: 'no-cache',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      const result = await response.json();
+      setBlogPosts(result.data);
+      setCount(result.itemsCount);
+    };
+    fetchData();
   }, [offset]);
   return (
     <>
-      <Wrapper>
-        <Feed tag={tag} setTag={setTag} />
-        {blogPosts.map((post) => (
-          <Post data={post} key={post.id} />
-        ))}
-        <Pages setOffset={setOffset} offset={offset} count={count} />
-      </Wrapper>
-      <Tags
-        setBlogPosts={setBlogPosts}
-        setCount={setCount}
-        offset={offset}
-        setTag={setTag}
-      />
+      {blogPosts ? (
+        <>
+          <Wrapper>
+            <Feed
+              tag={tag}
+              setTag={setTag}
+              setBlogPosts={setBlogPosts}
+              setCount={setCount}
+              offset={offset}
+            />
+            {blogPosts.map((post) => (
+              <Post data={post} key={post.id} />
+            ))}
+            <Pages setOffset={setOffset} offset={offset} count={count} />
+          </Wrapper>
+          <Tags
+            setBlogPosts={setBlogPosts}
+            setCount={setCount}
+            offset={offset}
+            setTag={setTag}
+          />
+        </>
+      ) : (
+        <p>Loading</p>
+      )}
     </>
   );
 }
--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -3,9 +3,28 @@ import { Item, Wrapper, List } from 'components/Feed/Feed.style';
 
 function Feed(props) {
   // eslint-disable-next-line react/prop-types
-  const { tag, setTag } = props;
+  const { tag, setTag, setBlogPosts } = props;
+  // eslint-disable-next-line react/prop-types
+  const { setCount, offset } = props;
   const toggle = () => {
-    setTag(null);
+    const fetchData = async () => {
+      const response = await fetch(
+        `http://localhost:4000/api/v1/blog?limit=5&offset=${offset}`,
+        {
+          method: 'GET',
+          mode: 'cors',
+          cache: 'no-cache',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      const result = await response.json();
+      setTag(null);
+      setBlogPosts(result.data);
+      setCount(result.itemsCount);
+    };
+    fetchData();
   };
   return (
     <Wrapper>
